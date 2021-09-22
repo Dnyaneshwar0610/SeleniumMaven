571e83c09c8703a25a5094e674657b021ed31f60
unknown <mrdnyaneshwarkamble@gmail.com>
2021-09-22 17:15:50 +0530

    https://github.com/Dnyaneshwar0610/SeleniumMaven.git

---
+++ src/test/java/com/maven/MavenTest.java
@@ -0,0 +1,8 @@
+package com.maven;
+
+public class MavenTest {
+
+	public static void main(String[] args) {
+		System.out.println("my maven simple project");
+	}
+}
---
+++ src/test/java/com/maven/Test.java
@@ -0,0 +1,10 @@
+package com.maven;
+
+public class Test {
+	public static void main(String[] args) {
+	System.out.println("my maven Test Class");
+}
+}
+
+
+
